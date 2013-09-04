e9a6c49cc01ddff397947a5825c8f3b0ddb6a47f
Brandon Amos <bdamos@vt.edu>
2013-09-04 18:04:46 -0400

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -7,36 +7,34 @@
 # Brandon Amos
 # 2013.01.20
 
-function echo_l1 { echo "    $@"; }
-function echo_l2 { echo_l1 "    $@"; }
+function echo_l1 { echo "  $@"; }
+function echo_l2 { echo "    $@"; }
 
 function link_file() {
-    local ORIG="$1"
-    local NEW="$2"
+  local ORIG="$1"; local NEW="$2"
 
-    echo_l1 "'$ORIG'"
-    if [[ $BACKUP =~ ^[Yy]$ ]]; then
-        mv "$ORIG" "${ORIG}.prebamos" &> /dev/null \
-        && echo_l2 "...backed up"
-    else
-        rm -rf "$ORIG"
-        echo_l2 "...deleted"
-    fi
-    ln -s "$NEW" "$ORIG" \
-        && echo_l2 "...linked"
+  echo_l1 "'$ORIG'"
+  if [[ $BACKUP =~ ^[Yy]$ ]]; then
+    mv "$ORIG" "${ORIG}.prebamos" &> /dev/null \
+    && echo_l2 "...backed up"
+  else
+    rm -f "$ORIG"
+    echo_l2 "...deleted"
+  fi
+  ln -s "$NEW" "$ORIG" && echo_l2 "...linked"
 }
 
 function sync_dotfiles() {
-    echo "Symlinking..."
-    DOTFILES="$(find . -maxdepth 1 -name '.?*')" # ?* - Don't include ./.
+  echo "Symlinking..."
+  DOTFILES="$(find . -maxdepth 1 -name '.?*')" # ?* - Don't include ./.
 
-    for DOTFILE in $DOTFILES; do
-        [[ $DOTFILE != "./.git" ]] \
-            && [[ $DOTFILE != "./.gitmodules" ]] \
-            && [[ $DOTFILE != "./.gitignore" ]] \
-            && [[ ! $DOTFILE =~ swp$ ]] \
-            && link_file "$HOME/$DOTFILE" "$CHECKOUT_DIR/$DOTFILE"
-    done
+  for DOTFILE in $DOTFILES; do
+    [[ $DOTFILE != "./.git" ]] \
+      && [[ $DOTFILE != "./.gitmodules" ]] \
+      && [[ $DOTFILE != "./.gitignore" ]] \
+      && [[ ! $DOTFILE =~ swp$ ]] \
+      && link_file "$HOME/$DOTFILE" "$CHECKOUT_DIR/$DOTFILE"
+  done
 }
 
 #########
