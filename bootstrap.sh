8a5d9e8e87ac57d1aa54b41ed5994109a1a5ffd4
Brandon Amos <bdamos@vt.edu>
2013-12-14 03:04:23 -0500

    Update bootstrap script.

--- bootstrap.sh
+++ bootstrap.sh
@@ -7,24 +7,21 @@
 # Brandon Amos
 # 2013.01.20
 
-function echo_l1 { echo "  $@"; }
-function echo_l2 { echo "    $@"; }
-
-function link_file() {
+link_file() {
   local ORIG="$1"; local NEW="$2"
 
-  echo_l1 "'$ORIG'"
+  echo "  '$ORIG'"
   if [[ $BACKUP =~ ^[Yy]$ ]]; then
     mv "$ORIG" "${ORIG}.prebamos" &> /dev/null \
-    && echo_l2 "...backed up"
+    && echo "    ...backed up"
   else
     rm -f "$ORIG"
-    echo_l2 "...deleted"
+    echo "    ...deleted"
   fi
   ln -s "$NEW" "$ORIG" && echo_l2 "...linked"
 }
 
-function sync_dotfiles() {
+sync_dotfiles() {
   echo "Symlinking..."
   DOTFILES="$(find . -maxdepth 1 -name '.?*')" # ?* - Don't include ./.
 
@@ -36,10 +33,6 @@ function sync_dotfiles() {
       && link_file "$HOME/$DOTFILE" "$CHECKOUT_DIR/$DOTFILE"
   done
 }
-
-#########
-# Start #
-#########
 
 read -p "Backup files? (y/n) " -n 1; echo
 BACKUP=$REPLY
