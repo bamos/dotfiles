89907ac33d0b311de00d3e63a805ee7ca14791e2
Brandon Amos <bdamos@vt.edu>
2014-04-16 11:23:51 -0400

    MYPID->MPV_PID

--- .mpv/delete.sh
+++ .mpv/delete.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
 # On multiuser machines, output and grep on $USER too
-MYPID=$(ps axo '%p %c'|grep [m]pv$|awk '{print $1}')
+MPV_PID=$(ps axo '%p %c'|grep [m]pv$|awk '{print $1}')
 
-if [ "$(echo ${MYPID}|wc -w)" -ne 1 ] ; then
-  echo "#no safe output: too many PIDs: \"${MYPID}\" ($(echo ${MYPID}|wc -w))"
+if [ "$(echo ${MPV_PID}|wc -w)" -ne 1 ] ; then
+  echo "Error: too many mpv PIDs: \"${MPV_PID}\" ($(echo ${MPV_PID}|wc -w))"
   exit 1
 fi
 
 IFS='
 '
-for FILE in $(lsof -p ${MYPID} -Ftn |grep -A1 ^tREG|grep ^n|sed 's/^n//g'); do
-  if test -w "${FILE}" ; then
-    mv "${FILE}" /tmp/mpv-deleted
-    echo "${FILE}" > /tmp/mpv-deleted-name
-    echo "${FILE} deleted."
+for F in $(lsof -p ${MPV_PID} -Ftn |grep -A1 ^tREG|grep ^n|sed 's/^n//g'); do
+  if test -w "$F" ; then
+    mv "$F" /tmp/mpv-deleted
+    echo "$F" > /tmp/mpv-deleted-name
+    echo "$F deleted."
   fi
 done
