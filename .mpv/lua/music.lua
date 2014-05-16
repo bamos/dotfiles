2101d5871942b3597b0c2a5788413e59c1f30e86
Brandon Amos <bdamos@vt.edu>
2014-05-15 22:19:41 -0400

    music.lua: Add comments.

--- .mpv/lua/music.lua
+++ .mpv/lua/music.lua
@@ -1,16 +1,34 @@
+-- music.lua
+--
+-- Brandon Amos <http://bamos.io/>
+-- 2014.05.15
 require 'os'
 require 'io'
 require 'string'
 
+-- Helper function to execute a command and return the output as a string.
+-- Reference: http://stackoverflow.com/questions/132397
+function os.capture(cmd, raw)
+  local f = assert(io.popen(cmd, 'r'))
+  local s = assert(f:read('*a'))
+  f:close()
+  return string.sub(s, 0, -2) -- Use string.sub to trim the trailing newline.
+end
+
+-- Global variables for deleting/restoring the current track.
 deleted_tmp = "/tmp/mpv-deleted"
 last_deleted_track = ""
 
+-- Delete the current track by moving it to the `deleted_tmp` location.
 function delete_current_track()
   last_deleted_track = mp.get_property("path")
   os.execute("mv '" .. last_deleted_track .. "' '" .. deleted_tmp .. "'")
   print("'" .. last_deleted_track .. "' deleted.")
 end
 
+-- Restore the last deleted track.
+-- This can be done to restore an arbitrary number of tracks by
+-- using a queue rather than a single file.
 function restore_prev_track()
   if last_deleted_track ~= "" then
     os.execute("mv '" .. deleted_tmp .. "' '" .. last_deleted_track .. "'")
@@ -21,29 +39,21 @@ function restore_prev_track()
   end
 end
 
+-- Move the current track into a `good` directory.
 function mark_good()
-  -- Move the current song into a `good` directory.
   last_deleted_track = mp.get_property("path")
   os.execute("mkdir -p good")
   os.execute("mv '" .. mp.get_property("path") .. "' '" .. "good" .. "'")
   print("Marked '" .. last_deleted_track .. "' as good.")
 end
 
-function os.capture(cmd, raw)
-  -- http://stackoverflow.com/questions/132397
-  local f = assert(io.popen(cmd, 'r'))
-  local s = assert(f:read('*a'))
-  f:close()
-  return string.sub(s, 0, -2) -- Use string.sub to trim the trailing newline.
-end
-
+-- Print the current track's artist and title in the following format.
+--
+-- [music] ---------------
+-- [music] Title: Marche Slave
+-- [music] Artist: Tchaikovsky
+-- [music] ---------------
 function print_info()
-  -- Print the current track's artist and title in the following format.
-  --
-  -- [music] ---------------
-  -- [music] Title: Marche Slave
-  -- [music] Artist: Tchaikovsky
-  -- [music] ---------------
   local info = os.capture(
     'exiftool -json ' .. mp.get_property("path") ..
     ' | grep \'^ *"\\(Artist\\|Title\\)\' ' ..
@@ -54,6 +64,9 @@ function print_info()
   print(string.rep("-", 15))
 end
 
+-- Use zenity and mutt to share the current track's information with a friend.
+-- This can be modified to send the message with any command-line email client
+-- or used to interface directly with an SMTP server.
 function share_info()
   local email = os.capture(
     'zenity --entry --title "Email to share with?" --text ""'
@@ -80,6 +93,7 @@ function share_info()
     "mutt -s '" .. info .. "'" .. " -- '" .. email .. "'")
 end
 
+-- Set key bindings.
 mp.add_key_binding("d", "delete_current_track", delete_current_track)
 mp.add_key_binding("r", "restore_prev_track", restore_prev_track)
 mp.add_key_binding("g", "mark_good", mark_good)
