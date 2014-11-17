1916e7befa4cb7ffe19a42772e8b612c58993e3b
Brandon Amos <bamos@cmu.edu>
2014-11-17 06:09:37 -0500

    Disable clipboard for OSX copy/paste with evil.

--- .emacs.d/clipboard.el
+++ .emacs.d/clipboard.el
@@ -1,12 +1,12 @@
-(when (eq system-type 'darwin)
-  (progn
-    (defun copy-from-osx () (let ((tramp-mode nil) (default-directory "~"))
-                              (shell-command-to-string "pbpaste")))
-    (defun paste-to-osx (text &optional push)
-      (let ((process-connection-type nil))
-        (let ((proc (start-process "pbcopy" "*Messages*" "pbcopy")))
-          (process-send-string proc text)
-          (process-send-eof proc))))
+;; (when (eq system-type 'darwin)
+;;   (progn
+;;     (defun copy-from-osx () (let ((tramp-mode nil) (default-directory "~"))
+;;                               (shell-command-to-string "pbpaste")))
+;;     (defun paste-to-osx (text &optional push)
+;;       (let ((process-connection-type nil))
+;;         (let ((proc (start-process "pbcopy" "*Messages*" "pbcopy")))
+;;           (process-send-string proc text)
+;;           (process-send-eof proc))))
 
-    (setq interprogram-cut-function 'paste-to-osx)
-    (setq interprogram-paste-function 'copy-from-osx)))
+;;     (setq interprogram-cut-function 'paste-to-osx)
+;;     (setq interprogram-paste-function 'copy-from-osx)))
