a57da29db090903545bc8c4f05034f5c18bf5504
Brandon Amos <bdamos@vt.edu>
2014-11-20 14:46:54 -0500

    Add rename-file-and-buffer.

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -76,3 +76,19 @@
   (clipboard-kill-ring-save (point-min) (point-max))
   (message "Copied to clipboard."))
 (global-set-key (kbd "C-c C-a") 'copy-all)
+
+(defun rename-file-and-buffer (new-name)
+  "Renames both current buffer and file it's visiting to NEW-NAME."
+  (interactive "sNew name: ")
+  (let ((name (buffer-name))
+        (filename (buffer-file-name)))
+    (if (not filename)
+        (message "Buffer '%s' is not visiting a file!" name)
+      (if (get-buffer new-name)
+          (message "A buffer named '%s' already exists!" new-name)
+        (progn
+          (rename-file name new-name 1)
+          (rename-buffer new-name)
+          (set-visited-file-name new-name)
+          (set-buffer-modified-p nil))))))
+(global-set-key (kbd "C-c C-r") 'rename-file-and-buffer)
