d942a6f16a01cc1d11064e71636f895e9a45a644
Brandon Amos <bdamos@vt.edu>
2014-11-23 03:33:14 -0500

    Add basic xmonad config.

---
+++ .xmonad/xmonad.hs
@@ -0,0 +1,50 @@
+import System.IO
+import System.Exit
+import XMonad
+import XMonad.Hooks.DynamicLog
+import XMonad.Hooks.ManageDocks
+import XMonad.Hooks.ManageHelpers
+import XMonad.Hooks.SetWMName
+import XMonad.Layout.Fullscreen
+import XMonad.Layout.NoBorders
+import XMonad.Layout.Spiral
+import XMonad.Layout.Tabbed
+import XMonad.Layout.ThreeColumns
+import XMonad.Util.Run(spawnPipe)
+import XMonad.Util.EZConfig(additionalKeys)
+import qualified XMonad.StackSet as W
+import qualified Data.Map        as M
+
+
+myLayout = avoidStruts (
+    Tall 1 (3/100) (1/2) |||
+    Mirror (Tall 1 (3/100) (1/2)) |||
+    tabbed shrinkText tabConfig |||
+    Full |||
+    spiral (6/7)) |||
+    noBorders (fullscreenFull Full)
+
+
+tabConfig = defaultTheme {
+    activeBorderColor = "#7C7C7C",
+    activeTextColor = "#CEFFAC",
+    activeColor = "#000000",
+    inactiveBorderColor = "#7C7C7C",
+    inactiveTextColor = "#EEEEEE",
+    inactiveColor = "#000000"
+}
+
+main = do
+  xmonad $ defaults {
+      logHook = dynamicLogWithPP
+      , startupHook = setWMName "LG3D"
+  }
+
+
+defaults = defaultConfig {
+    terminal = "/usr/bin/urxvt",
+    modMask = mod4Mask,
+    normalBorderColor  = "#333333",
+    focusedBorderColor = "#5882FA",
+    layoutHook = smartBorders $ myLayout
+}
