ea62007c221c2dc2a452b4ea95bdf5350444ffca
Brandon Amos <bdamos@vt.edu>
2015-08-10 16:27:41 -0400

    Emacs: W3m: Change default search engine.

--- .emacs.d/init-w3m.el
+++ .emacs.d/init-w3m.el
@@ -6,7 +6,11 @@
   (setq w3m-use-toolbar nil)
   (setq w3m-command-arguments '("-M"))
   (setq w2m-use-cookies t)
-  (setq w3m-search-default-engine "google")
+
+  (require 'w3m-search)
+  (setq w3m-search-default-engine "duckduckgo")
+  (add-to-list 'w3m-search-engine-alist
+               '("duckduckgo" "http://duckduckgo.com/html/?q=%s" nil))
 
   (setq w3m-user-agent "Mozilla/5.0 (Linux; U; Android 2.3.3; zh-tw; HTC_Pyramid Build/GRI40) AppleWebKit/533.1 (KHTML, like Gecko) Version/4.0 Mobile Safari/533.")
 
