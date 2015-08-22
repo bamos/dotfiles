1f00a8161259c4ae45b7c38d57305fa07e7dd9ff
Brandon Amos <bamos@cs.cmu.edu>
2015-08-21 21:08:50 -0400

    Add list subdir by files.

--- .funcs/if-linux.sh
+++ .funcs/if-linux.sh
@@ -12,3 +12,9 @@ alias m="make -j$(cat /proc/cpuinfo | grep -c processor)"
 alias za='zathura'
 alias less='/usr/share/vim/vim74/macros/less.sh'
 alias vim='vim -p'
+
+
+# http://stackoverflow.com/questions/6918057
+list-subdir-by-files() {
+  find . -xdev -type f | cut -d "/" -f 2 | sort | uniq -c | sort -n
+}
