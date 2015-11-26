2cf3dfd8b3fbdd9f9f9d7588f6ef615589aa4c86
Brandon Amos <bamos@cs.cmu.edu>
2015-11-26 12:14:20 -0500

    Funcs: Add 2x2-slides.

--- .funcs/misc.sh
+++ .funcs/misc.sh
@@ -137,6 +137,42 @@ ls-by-files() {
   find . -xdev -type f | cut -d "/" -f 2 | sort | uniq -c | sort -n
 }
 
+# Takes a PDF of slides as input and outputs them tiled
+# in a 2x2 landscape PDF.
+2x2-slides() {
+  local INPUT_SLIDES=$1
+  local OUTPUT=2x2-$INPUT_SLIDES
+
+  # LaTeX has trouble with some filenames.
+  ln -sf $PWD/$INPUT_SLIDES /tmp/slides.pdf
+  INPUT_SLIDES=/tmp/slides.pdf
+
+  cat>/tmp/2x2-slides.tex<<EOF
+\documentclass[a4paper]{article}
+\usepackage[T1]{fontenc}
+\usepackage[utf8]{inputenc}
+\usepackage{pdfpages}
+\usepackage{hyperref}
+
+\begin{document}
+
+\hypersetup{}
+
+\pdfbookmark[0]{landscape}{1}
+
+\includepdfmerge[nup=2x2, column=False, frame=False, angle=0,
+                 scale=1, landscape=True, noautoscale=False,
+                 clip=True ]{$INPUT_SLIDES, -}
+
+
+\end{document}
+EOF
+  rm -f /tmp/2x2-slides.pdf
+  pdflatex /tmp/2x2-slides.tex > /dev/null
+  mv 2x2-slides.pdf $OUTPUT
+  echo "Output in $OUTPUT"
+}
+
 alias c='clear'
 alias chax='chmod a+x'
 alias h='hostname'
