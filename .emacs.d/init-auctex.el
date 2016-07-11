dcd9b56a0f50926136e8a656d79d3cf24326eb0a
Brandon Amos <bamos@cs.cmu.edu>
2016-07-11 19:28:01 -0400

    Add LateX tools to PATH for auctex.

--- .emacs.d/init-auctex.el
+++ .emacs.d/init-auctex.el
@@ -5,7 +5,9 @@
 (if (eq system-type 'darwin)
   (progn
     (setq TeX-view-program-list '(("open" "open %o")))
-    (setq TeX-view-program-selection '((output-pdf "open"))))
+    (setq TeX-view-program-selection '((output-pdf "open")))
+    (setenv "PATH" (concat (getenv "PATH") "/Library/TeX/texbin:/usr/local/bin"))
+    (setq exec-path (append exec-path '("/Library/TeX/texbin" "/usr/local/bin"))))
   (progn
     (setq TeX-view-program-list '(("zathura" "zathura %o")))
     (setq TeX-view-program-selection '((output-pdf "zathura")))))
