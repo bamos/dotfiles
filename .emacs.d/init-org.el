e175e9f57d1d18f54283a4383fa5deca5e635b01
Brandon Amos <bamos@cs.cmu.edu>
2016-07-15 11:02:41 -0400

    org-mode: Add automatic link titles.
    Thanks @pkkm! This is very useful.

--- .emacs.d/init-org.el
+++ .emacs.d/init-org.el
@@ -12,5 +12,36 @@
 
   (local-set-key (kbd "C-c a") 'org-agenda)
 
-  (local-set-key (kbd "C-S-<return>") 'org-insert-heading-after-current)
-  (org-display-inline-images)))
+  (local-set-key (kbd "C-S-<return>") 'org-insert-heading-after-current)))
+
+(org-display-inline-images)
+
+;; Automatic link description downloading.
+;; Source: https://github.com/pkkm/.emacs.d/blob/8ac745bae09e303a13708fb9260849de9e1d8502/conf/mode-specific/org.el#L90
+
+; (use-package s :ensure t :commands s-trim s-collapse-whitespace)
+(autoload 'mm-url-decode-entities-string "mm-url")
+(defun get-url-html-title (url &rest ignored)
+"Return the title of the HTML page at URL."
+(let ((download-buffer (url-retrieve-synchronously url))
+        title-start title-end)
+    (save-excursion
+    (set-buffer download-buffer)
+    (beginning-of-buffer)
+    (setq title-start (search-forward "<title>"))
+    (search-forward "</title>")
+    (setq title-end (search-backward "<"))
+    (s-trim
+        (s-collapse-whitespace
+        (mm-url-decode-entities-string
+        (buffer-substring-no-properties title-start title-end)))))))
+
+(defun my-org-toggle-auto-link-description ()
+"Toggle automatically downloading link descriptions."
+(interactive)
+(if org-make-link-description-function
+    (progn
+        (setq org-make-link-description-function nil)
+        (message "Automatic link description downloading disabled."))
+    (setq org-make-link-description-function #'get-url-html-title)
+    (message "Automatic link description downloading enabled.")))
