eb5cf52bc416034b9ef6693bd3cceac914cc50ca
Brandon Amos <brandon.amos.cs@gmail.com>
2019-10-15 16:26:34 -0400

    hammerspoon: Tweak around.

--- .hammerspoon/init.lua
+++ .hammerspoon/init.lua
@@ -1,45 +1,117 @@
+local grid = require "hs.grid"
+
 local mash      = {"cmd", "alt", "ctrl"}
 local mashshift = {"cmd", "alt", "shift"}
 local funkymash = {"cmd", "ctrl", "shift"}
 
--- Set grid size.
 hs.grid.GRIDWIDTH  = 12
 hs.grid.GRIDHEIGHT = 12
 hs.grid.MARGINX    = 0
 hs.grid.MARGINY    = 0
--- Set window animation off. It's much smoother.
 hs.window.animationDuration = 0
--- Set volume increments
 local volumeIncrement = 5
 
--- hs.hotkey.bind(mash, ';', function() hs.grid.snap(hs.window.focusedWindow()) end)
--- hs.hotkey.bind(mash, "'", function() hs.fnutils.map(hs.window.visibleWindows(), hs.grid.snap) end)
+hs.hotkey.bind(mash, '-', function() hs.grid.snap(hs.window.focusedWindow()) end)
+hs.hotkey.bind(mash, "z", function() hs.fnutils.map(hs.window.visibleWindows(), hs.grid.snap) end)
 
-hs.hotkey.bind(mash, 'h',
-               function() hs.window.focusedWindow():focusWindowWest()  end)
-hs.hotkey.bind(mash, 's',
-               function() hs.window.focusedWindow():focusWindowEast()  end)
-hs.hotkey.bind(mash, 'n',
-               function() hs.window.focusedWindow():focusWindowNorth() end)
-hs.hotkey.bind(mash, 't',
-               function() hs.window.focusedWindow():focusWindowSouth() end)
+hs.hotkey.bind(mashshift, 'h', grid.pushWindowLeft)
+hs.hotkey.bind(mashshift, 't', grid.pushWindowDown)
+hs.hotkey.bind(mashshift, 'n', grid.pushWindowUp)
+hs.hotkey.bind(mashshift, 's', grid.pushWindowRight)
+
+-- hotkey.bind(mashshift, 'UP', grid.resizeWindowShorter)
+-- hotkey.bind(mashshift, 'DOWN', grid.resizeWindowTaller)
+-- hotkey.bind(mashshift, 'RIGHT', grid.resizeWindowWider)
+-- hotkey.bind(mashshift, 'LEFT', grid.resizeWindowThinner)
+
+hs.hotkey.bind(mash, 'h', function() hs.window.focusedWindow():focusWindowWest()  end)
+hs.hotkey.bind(mash, 's', function() hs.window.focusedWindow():focusWindowEast()  end)
+hs.hotkey.bind(mash, 'n', function() hs.window.focusedWindow():focusWindowNorth() end)
+hs.hotkey.bind(mash, 't', function() hs.window.focusedWindow():focusWindowSouth() end)
+hs.hotkey.bind(mash, 'l', function() hs.reload() end)
+
+hs.hotkey.bind(mash, 'g', function()
+  local screens = hs.screen.allScreens()
+
+  -- Left screen
+  local screen = screens[1]
+  local max = screen:frame()
+
+  -- local win = hs.application.get("Google Chrome"):allWindows()[3]
+  -- win:moveToScreen(screen)
+  -- local f = win:frame()
+  -- local chrome_width = 1300
+  -- f.x = max.w-chrome_width
+  -- f.y = max.y
+  -- f.w = chrome_width
+  -- f.h = max.h
+  -- win:setFrame(f)
+
+  -- Right screen
+  local screen = screens[2]
+  local max = screen:frame()
+
+  local iTerms = hs.application.get("iTerm2"):allWindows()
+  local win = iTerms[1]
+  local term_width = 700
+  win:moveToScreen(screen)
+  local f = win:frame()
+  f.x = max.x
+  f.y = max.y
+  f.w = term_width
+  f.h = max.h
+  win:setFrame(f)
+
+  local win = iTerms[2]
+  win:moveToScreen(screen)
+  local f = win:frame()
+  f.x = max.x+term_width-5
+  f.y = max.y
+  f.w = term_width
+  f.h = max.h
+  win:setFrame(f)
+
+  local win = hs.application.get("Emacs"):allWindows()[1]
+  win:moveToScreen(screen)
+  local f = win:frame()
+  f.x = max.x+2*(term_width-5)
+  f.y = max.y
+  f.w = term_width
+  f.h = max.h
+  win:setFrame(f)
+end)
+
+-- hs.hotkey.bind(mash, 'm', function()
+--   local win = hs.window.focusedWindow()
+--   local f = win:frame()
+--   local screen = win:screen()
+--   local max = screen:frame()
+
+--   f.x = max.x
+--   f.y = max.y
+--   f.w = max.w / 2
+--   f.h = max.h
+--   win:setFrame(f)
+-- end)
+
+-- hs.hotkey.bind(mash, 'w', function()
+--   local win = hs.window.focusedWindow()
+--   local f = win:frame()
+--   local screen = win:screen()
+--   local max = screen:frame()
+
+--   f.x = max.x + max.w / 2
+--   f.y = max.y
+--   f.w = max.w / 2
+--   f.h = max.h
+--   win:setFrame(f)
+-- end)
 
 -- hs.hotkey.bind(mash, 'M', hs.grid.maximizeWindow)
-
 -- hs.hotkey.bind(mash, 'F', function() hs.window.focusedWindow():toggleFullScreen() end)
 
 -- hs.hotkey.bind(mash, 'N', hs.grid.pushWindowNextScreen)
 -- hs.hotkey.bind(mash, 'P', hs.grid.pushWindowPrevScreen)
-
--- hs.hotkey.bind(mash, 'J', hs.grid.pushWindowDown)
--- hs.hotkey.bind(mash, 'K', hs.grid.pushWindowUp)
--- hs.hotkey.bind(mash, 'H', hs.grid.pushWindowLeft)
--- hs.hotkey.bind(mash, 'L', hs.grid.pushWindowRight)
-
--- hs.hotkey.bind(mash, 'U', hs.grid.resizeWindowTaller)
--- hs.hotkey.bind(mash, 'O', hs.grid.resizeWindowWider)
--- hs.hotkey.bind(mash, 'I', hs.grid.resizeWindowThinner)
--- hs.hotkey.bind(mash, 'Y', hs.grid.resizeWindowShorter)
 
 hs.hotkey.bind(mashshift, 'space', hs.spotify.displayCurrentTrack)
 hs.hotkey.bind(mash, 'space', hs.spotify.playpause)
