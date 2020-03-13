4bf4d216a1ce1491ea7256893b3579d2b808a93d
Brandon Amos <brandon.amos.cs@gmail.com>
2020-03-13 16:48:58 -0500

    tweak xmonad formatting

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -37,11 +37,16 @@ _keys conf@(XConfig {XMonad.modMask = mod}) = M.fromList $
     , ((mod, xK_s), spawn "slock")
     -- , ((mod .|. shiftMask, xK_Up), spawn "amixer set Master playback 5%+")
     -- , ((mod .|. shiftMask, xK_Down), spawn "amixer set Master playback 5%-")
-    , ((mod .|. shiftMask, xK_Up), spawn "ssh host ./.dotfiles/bin/bump-osx-volume ++")
-    , ((mod .|. shiftMask, xK_Down), spawn "ssh host ./.dotfiles/bin/bump-osx-volume --")
-    , ((mod .|. shiftMask, xK_Right), spawn "dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.Next")
-    , ((mod .|. shiftMask, xK_Left), spawn "dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.Prev")
-    , ((mod .|. shiftMask, xK_u), spawn "dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.PlayPause")
+    , ((mod .|. shiftMask, xK_Up),
+       spawn "ssh host ./.dotfiles/bin/bump-osx-volume ++")
+    , ((mod .|. shiftMask, xK_Down),
+       spawn "ssh host ./.dotfiles/bin/bump-osx-volume --")
+    , ((mod .|. shiftMask, xK_Right),
+       spawn "dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.Next")
+    , ((mod .|. shiftMask, xK_Left),
+       spawn "dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.Prev")
+    , ((mod .|. shiftMask, xK_u),
+       spawn "dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.PlayPause")
     , ((0, 0x1008FF11), spawn "amixer set Master playback 5%-")
     , ((0, 0x1008FF13), spawn "amixer set Master playback 5%+")
     -- , ((mod, xK_Right), spawn "cmus-remote -n")
@@ -58,64 +63,61 @@ _keys conf@(XConfig {XMonad.modMask = mod}) = M.fromList $
     , ((mod, xK_l), spawn "dmenu_run")
     , ((mod.|. shiftMask, xK_j), kill)
 
-    , ((mod, xK_space), sendMessage NextLayout) -- %! Rotate through the available layout algorithms
-    , ((mod.|. shiftMask, xK_space), setLayout $ XMonad.layoutHook conf) -- %!  Reset the layouts on the current workspace to default
-
-    -- , ((mod, xK_b), refresh) -- %! Resize viewed windows to the correct size
+    , ((mod, xK_space), sendMessage NextLayout)
 
     -- Move focus up or down the window stack.
-    , ((mod, xK_Tab), windows W.focusDown) -- %! Move focus to the next window
-    , ((mod.|. shiftMask, xK_Tab), windows W.focusUp) -- %! Move focus to the previous window
-    , ((mod, xK_h), windows W.focusDown) -- %! Move focus to the next window
-    , ((mod, xK_t), windows W.focusUp) -- %! Move focus to the previous window
-    , ((mod, xK_m), windows W.focusMaster) -- %! Move focus to the master window
+    , ((mod, xK_Tab), windows W.focusDown)
+    , ((mod.|. shiftMask, xK_Tab), windows W.focusUp)
+    , ((mod, xK_h), windows W.focusDown)
+    , ((mod, xK_t), windows W.focusUp)
+    , ((mod, xK_m), windows W.focusMaster)
 
     -- Modifying the window order.
-    , ((mod, xK_Return), windows W.swapMaster) -- %! Swap the focused window and the master window
-    , ((mod.|. shiftMask, xK_h), windows W.swapDown) -- %! Swap the focused window with the next window
-    , ((mod.|. shiftMask, xK_t), windows W.swapUp) -- %! Swap the focused window with the previous window
+    , ((mod, xK_Return), windows W.swapMaster)
+    , ((mod.|. shiftMask, xK_h), windows W.swapDown)
+    , ((mod.|. shiftMask, xK_t), windows W.swapUp)
 
-    -- Resizing the master/slave ratio.
-    , ((mod, xK_d), sendMessage Shrink) -- %! Shrink the master area
-    , ((mod, xK_n), sendMessage Expand) -- %! Expand the master area
+    -- Resizing the master.
+    , ((mod, xK_d), sendMessage Shrink)
+    , ((mod, xK_n), sendMessage Expand)
 
     -- Floating layer support.
-    , ((mod, xK_y), withFocused $ windows . W.sink) -- %! Push window back into tiling
+    , ((mod, xK_y), withFocused $ windows . W.sink)
 
     -- Screen division.
-    , ((mod .|. shiftMask,                 xK_space), layoutScreens 2 (TwoPane 0.55 0.45))
+    , ((mod .|. shiftMask, xK_space), layoutScreens 2 (TwoPane 0.55 0.45))
     , ((mod .|. controlMask .|. shiftMask, xK_space), rescreen)
 
     -- Increase or decrease number of windows in the master area.
     -- , ((mod, xK_w), sendMessage (IncMasterN 1))
     -- , ((mod, xK_v), sendMessage (IncMasterN (-1)))
 
-    -- Quit, or restart.
+    -- Quit or restart.
     , ((mod .|. shiftMask, xK_apostrophe), io exitSuccess)
     , ((mod, xK_apostrophe), spawn "xmonad --recompile && xmonad --restart")
-    ]
-    ++
-    -- mod-[1..9] %! Switch to workspace N
-    -- mod-shift-[1..9] %! Move client to workspace N
+    ] ++
+
+    -- mod-[1..9]: Switch to workspace N
+    -- mod-shift-[1..9]: Move client to workspace N
     [((m .|. mod, k), windows $ f i)
         | (i, k) <- zip (XMonad.workspaces conf) [xK_1 .. xK_9]
         , (f, m) <- [(W.greedyView, 0), (W.shift, shiftMask)]]
     ++
-    -- mod-{,,.} %! Switch to physical/Xinerama screens 1 or 2
-    -- mod-shift-{,,.} %! Move client to screen 1 or 2.
+
+    -- mod-{,,.}: Switch to physical/Xinerama screens 1 or 2
+    -- mod-shift-{,,.}: Move client to screen 1 or 2.
     [((m .|. mod, key), screenWorkspace sc >>= flip whenJust (windows . f))
         | (key, sc) <- zip [xK_comma, xK_period] [0..]
         , (f, m) <- [(W.view, 0), (W.shift, shiftMask)]]
 
-_config = defaultConfig { terminal = "/usr/bin/urxvt"
-                        , modMask = mod1Mask
-                        , keys = _keys
-                        , normalBorderColor  = "#333333"
-                        , focusedBorderColor = "#5882FA"
-                        , layoutHook = smartBorders _layout
-                        -- , startupHook = spawnOnce "xmobar ~/.xmonad/xmobar.hs" <+>
-                        --                 setWMName "LG3D"
-                        }
 
-
-main = xmonad _config
+main = xmonad defaultConfig
+  { terminal = "/usr/bin/urxvt"
+  , modMask = mod1Mask
+  , keys = _keys
+  , normalBorderColor  = "#333333"
+  , focusedBorderColor = "#5882FA"
+  , layoutHook = smartBorders _layout
+  -- , startupHook = spawnOnce "xmobar ~/.xmonad/xmobar.hs" <+>
+  --                 setWMName "LG3D"
+}
