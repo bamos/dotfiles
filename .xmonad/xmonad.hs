5e7ef4e4d05f7c600d3db5846c1b58f21e7b5c2c
Brandon Amos <brandon.amos.cs@gmail.com>
2020-03-14 09:41:29 -0500

    more xmonad tweaks, pull some spawns into functions

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -1,9 +1,6 @@
-import System.IO
 import System.Exit
 import XMonad
-import XMonad.Hooks.DynamicLog
 import XMonad.Hooks.ManageDocks
-import XMonad.Hooks.SetWMName
 import XMonad.Layout.Grid
 import XMonad.Layout.Fullscreen
 import XMonad.Layout.NoBorders
@@ -18,94 +15,76 @@ import qualified XMonad.StackSet as W
 import Data.Bits ((.|.))
 import qualified Data.Map as M
 
-_layout = avoidStruts (   Tall 1 (3/100) (1/2)
-                      ||| Mirror (Tall 1 (3/100) (1/2))
-                      ||| Full
-                      ||| Grid
-                      ||| spiral (6/7)
-                      )
-          |||
-          simpleFloat
 
--- Keymappings are for Dvorak.
--- Original: http://xmonad.org/xmonad-docs/xmonad/src/XMonad-Config.html
+spotify :: MonadIO m => String -> m()
+spotify cmd = spawn
+  (  "dbus-send --print-reply "
+  ++ "--dest=org.mpris.MediaPlayer2.spotify "
+  ++ "/org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player."
+  ++ cmd
+  )
+
+bumpVolume :: MonadIO m => String -> m()
+bumpVolume args = spawn ("ssh host ./.dotfiles/bin/bump-osx-volume " ++ args)
+
+setkbmap :: MonadIO m => String -> m()
+setkbmap layout = spawn ("setxkbmap " ++ layout ++ "; xmodmap ~/.Xmodmap")
+
+
+_layout = smartBorders (
+        avoidStruts
+            (   Tall 1 (3/100) (1/2)
+            ||| Mirror (Tall 1 (3/100) (1/2))
+            ||| Full
+            ||| Grid
+            ||| spiral (6/7)
+            )
+        ||| simpleFloat
+    )
+
+
 _keys :: XConfig Layout -> M.Map (KeyMask, KeySym) (X ())
 _keys conf@(XConfig {XMonad.modMask = mod}) = M.fromList $
-    -- Programs.
     [ ((mod, xK_o), spawn "chromium")
-    , ((mod, xK_semicolon), spawn "sleep 1; xset dpms force off")
-    , ((mod, xK_s), spawn "slock")
-    -- , ((mod .|. shiftMask, xK_Up), spawn "amixer set Master playback 5%+")
-    -- , ((mod .|. shiftMask, xK_Down), spawn "amixer set Master playback 5%-")
-    , ((mod .|. shiftMask, xK_Up),
-       spawn "ssh host ./.dotfiles/bin/bump-osx-volume ++")
-    , ((mod .|. shiftMask, xK_Down),
-       spawn "ssh host ./.dotfiles/bin/bump-osx-volume --")
-    , ((mod .|. shiftMask, xK_Right),
-       spawn "dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.Next")
-    , ((mod .|. shiftMask, xK_Left),
-       spawn "dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.Prev")
-    , ((mod .|. shiftMask, xK_u),
-       spawn "dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.PlayPause")
-    , ((0, 0x1008FF11), spawn "amixer set Master playback 5%-")
-    , ((0, 0x1008FF13), spawn "amixer set Master playback 5%+")
-    -- , ((mod, xK_Right), spawn "cmus-remote -n")
-    -- , ((mod, xK_Left), spawn "cmus-remote -r")
-    -- , ((mod, xK_u), spawn "cmus-remote -u")
-    , ((mod, xK_b), sendMessage ToggleStruts)
-
-    -- Change keyboard mappings.
-    , ((mod, xK_g), spawn "setxkbmap us; xmodmap ~/.Xmodmap")
-    , ((mod, xK_v), spawn "setxkbmap dvorak; xmodmap ~/.Xmodmap")
-
-    -- Launching and killing programs.
-    , ((mod.|. shiftMask, xK_Return), spawn $ XMonad.terminal conf)
+    , ((mod .|. shiftMask, xK_Return), spawn $ XMonad.terminal conf)
     , ((mod, xK_l), spawn "dmenu_run")
-    , ((mod.|. shiftMask, xK_j), kill)
+    , ((mod .|. shiftMask, xK_j), kill)
+
+    , ((mod, xK_Up), bumpVolume "++")
+    , ((mod, xK_Down), bumpVolume "--")
+    , ((mod, xK_Right), spotify "Next")
+    , ((mod, xK_Left), spotify "Prev")
+    , ((mod, xK_u), spotify "PlayPause")
+
+    , ((mod, xK_g), setkbmap "us")
+    , ((mod, xK_v), setkbmap "dvorak")
 
     , ((mod, xK_space), sendMessage NextLayout)
-
-    -- Move focus up or down the window stack.
-    , ((mod, xK_Tab), windows W.focusDown)
-    , ((mod.|. shiftMask, xK_Tab), windows W.focusUp)
-    , ((mod, xK_h), windows W.focusDown)
-    , ((mod, xK_t), windows W.focusUp)
-    , ((mod, xK_m), windows W.focusMaster)
-
-    -- Modifying the window order.
     , ((mod, xK_Return), windows W.swapMaster)
-    , ((mod.|. shiftMask, xK_h), windows W.swapDown)
-    , ((mod.|. shiftMask, xK_t), windows W.swapUp)
-
-    -- Resizing the master.
     , ((mod, xK_d), sendMessage Shrink)
     , ((mod, xK_n), sendMessage Expand)
-
-    -- Floating layer support.
     , ((mod, xK_y), withFocused $ windows . W.sink)
+    , ((mod, xK_h), windows W.focusDown)
+    , ((mod, xK_t), windows W.focusUp)
+    , ((mod .|. shiftMask, xK_h), windows W.swapDown)
+    , ((mod .|. shiftMask, xK_t), windows W.swapUp)
 
-    -- Screen division.
     , ((mod .|. shiftMask, xK_space), layoutScreens 2 (TwoPane 0.55 0.45))
     , ((mod .|. controlMask .|. shiftMask, xK_space), rescreen)
 
-    -- Increase or decrease number of windows in the master area.
-    -- , ((mod, xK_w), sendMessage (IncMasterN 1))
-    -- , ((mod, xK_v), sendMessage (IncMasterN (-1)))
-
-    -- Quit or restart.
-    , ((mod .|. shiftMask, xK_apostrophe), io exitSuccess)
     , ((mod, xK_apostrophe), spawn "xmonad --recompile && xmonad --restart")
+    , ((mod .|. shiftMask, xK_apostrophe), io exitSuccess)
     ] ++
 
-    -- mod-[1..9]: Switch to workspace N
-    -- mod-shift-[1..9]: Move client to workspace N
+    -- mod-N: Switch to workspace N
+    -- mod-shift-N: Move client to workspace N
     [((m .|. mod, k), windows $ f i)
         | (i, k) <- zip (XMonad.workspaces conf) [xK_1 .. xK_9]
         , (f, m) <- [(W.greedyView, 0), (W.shift, shiftMask)]]
     ++
 
-    -- mod-{,,.}: Switch to physical/Xinerama screens 1 or 2
-    -- mod-shift-{,,.}: Move client to screen 1 or 2.
+    -- mod-{,,.}: Switch to physical/Xinerama screens {1,2}
+    -- mod-shift-{,,.}: Move client to screen {1,2}
     [((m .|. mod, key), screenWorkspace sc >>= flip whenJust (windows . f))
         | (key, sc) <- zip [xK_comma, xK_period] [0..]
         , (f, m) <- [(W.view, 0), (W.shift, shiftMask)]]
@@ -117,7 +96,5 @@ main = xmonad defaultConfig
   , keys = _keys
   , normalBorderColor  = "#333333"
   , focusedBorderColor = "#5882FA"
-  , layoutHook = smartBorders _layout
-  -- , startupHook = spawnOnce "xmobar ~/.xmonad/xmobar.hs" <+>
-  --                 setWMName "LG3D"
+  , layoutHook = _layout
 }
