52faf4f6f52fc67c886d8dba6207b2969976f63d
Brandon Amos <brandon.amos.cs@gmail.com>
2020-03-14 18:31:03 -0500

    qutebrowser tweaks

--- .config/qutebrowser/config.py
+++ .config/qutebrowser/config.py
@@ -4,6 +4,11 @@ from qutebrowser.config.config import ConfigContainer  # noqa: F401
 config = config  # type: ConfigAPI # noqa: F821 pylint: disable=E0602,C0103
 c = c  # type: ConfigContainer # noqa: F821 pylint: disable=E0602,C0103
 
+import os
+SCRIPT_DIR = os.path.dirname(os.path.realpath(__file__))
+
+c.downloads.remove_finished = 0
+
 c.scrolling.bar = 'never'
 
 # c.spellcheck.languages = ['en-US']
@@ -16,6 +21,7 @@ c.tabs.favicons.show = 'never'
 c.tabs.show = 'never'
 c.tabs.title.format = '{index}: {current_title}'
 c.tabs.title.format_pinned = ''
+c.tabs.last_close = 'startpage'
 
 c.url.default_page = 'about:blank'
 c.url.open_base_url = True
@@ -24,7 +30,9 @@ c.url.searchengines = {
     'a': 'https://www.wolframalpha.com/input/?i={}',
     's': 'https://scholar.google.com/scholar?hl=en&q={}',
 }
-c.url.start_pages = ['about:blank']
+c.url.start_pages = ['file:///' + SCRIPT_DIR + '/index.html']
+c.colors.webpage.bg = '#262626'
+c.colors.webpage.prefers_color_scheme_dark = True
 
 c.window.hide_decoration = True
 
@@ -38,6 +46,9 @@ config.bind('h', 'scroll left')
 config.bind('s', 'scroll right')
 config.bind('t', 'scroll down')
 config.bind('n', 'scroll up')
+
+config.bind('H', 'back')
+config.bind('S', 'forward')
 
 config.bind('l', 'spawn --userscript qute-lastpass')
 
