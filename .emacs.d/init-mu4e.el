22f2cad41dad017ead940050da9f3fa70c6b1a45
Brandon Amos <brandon.amos.cs@gmail.com>
2020-03-16 11:39:04 -0400

    mu4e tweak

--- .emacs.d/init-mu4e.el
+++ .emacs.d/init-mu4e.el
@@ -5,39 +5,40 @@
 (global-set-key (kbd "C-x C-m") 'mu4e)
 
 (setq user-full-name "Brandon Amos"
-        user-mail-address "brandon.amos.cs@gmail.com"
-        message-send-mail-function 'smtpmail-send-it
-        smtpmail-local-domain "gmail.com"
-        smtpmail-default-smtp-server "smtp.gmail.com"
-        smtpmail-smtp-server "smtp.gmail.com"
-        smtpmail-smtp-service 587
-        smtpmail-queue-mail nil
-        smtpmail-queue-dir "~/.mu/queue/cur"
-        mu4e-maildir "~/.mbsync"
-        ;; mu4e-drafts-folder "/[Gmail].Drafts"
-        mu4e-sent-folder   "/[Gmail].Sent Mail"
-        mu4e-refile-folder   "/[Gmail].All Mail"
-        mu4e-trash-folder  "/[Gmail].Trash"
-        mu4e-sent-messages-behavior 'delete
-        ;; mu4e-mu-binary "/usr/local/Cellar/mu/1.2.0_1/bin/mu"
-        message-kill-buffer-on-exit t
-        mu4e-html2text-command "w3m -dump -T text/html -cols 72 -o display_link_number=true -o auto_image=false -o display_image=false -o ignore_null_img_alt=true"
-        mu4e-maildir-shortcuts
-        '( ("/INBOX"               . ?i)
-            ("/[Gmail].Sent Mail"   . ?s)
-            ("/[Gmail].Trash"       . ?t)
-            ;; ("/[Gmail].Drafts"      . ?d)
-            ("/[Gmail].All Mail"    . ?a))
-        mu4e-get-mail-command "mbsync gmail"
-        mu4e-update-interval (* 10 60)
-        mu4e-view-image-max-width 800
-        mu4e-view-show-images t
-        mu4e-view-show-addresses t
-        mu4e-change-filenames-when-moving t
-        mu4e-hide-index-messages 1
-        mu4e-confirm-quit nil
-        mu4e-headers-leave-behavior 'apply
-        mu4e-compose-complete-only-personal t)
+      user-mail-address "brandon.amos.cs@gmail.com"
+      mu4e-user-mail-address-list '("brandon.amos.cs@gmail.com")
+      message-send-mail-function 'smtpmail-send-it
+      smtpmail-local-domain "gmail.com"
+      smtpmail-default-smtp-server "smtp.gmail.com"
+      smtpmail-smtp-server "smtp.gmail.com"
+      smtpmail-smtp-service 587
+      smtpmail-queue-mail nil
+      smtpmail-queue-dir "~/.mu/queue/cur"
+      mu4e-maildir "~/.mbsync"
+      ;; mu4e-drafts-folder "/[Gmail].Drafts"
+      mu4e-sent-folder   "/[Gmail].Sent Mail"
+      mu4e-refile-folder   "/[Gmail].All Mail"
+      mu4e-trash-folder  "/[Gmail].Trash"
+      mu4e-sent-messages-behavior 'delete
+      ;; mu4e-mu-binary "/usr/local/Cellar/mu/1.2.0_1/bin/mu"
+      message-kill-buffer-on-exit t
+      mu4e-html2text-command "w3m -dump -T text/html -cols 72 -o display_link_number=true -o auto_image=false -o display_image=false -o ignore_null_img_alt=true"
+      mu4e-maildir-shortcuts
+      '( ("/INBOX"               . ?i)
+          ("/[Gmail].Sent Mail"   . ?s)
+          ("/[Gmail].Trash"       . ?t)
+        ;; ("/[Gmail].Drafts"      . ?d)
+          ("/[Gmail].All Mail"    . ?a))
+      mu4e-get-mail-command "mbsync gmail"
+      mu4e-update-interval (* 10 60)
+      mu4e-view-image-max-width 800
+      mu4e-view-show-images t
+      mu4e-view-show-addresses t
+      mu4e-change-filenames-when-moving t
+      mu4e-hide-index-messages 1
+      mu4e-confirm-quit nil
+      mu4e-headers-leave-behavior 'apply
+      mu4e-compose-complete-only-personal t)
 
 (when (fboundp 'imagemagick-register-types)
     (imagemagick-register-types))
