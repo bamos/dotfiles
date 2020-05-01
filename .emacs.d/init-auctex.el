ba657ce95d230453d0e8735210fc1fe969f52668
Brandon Amos <brandon.amos.cs@gmail.com>
2020-05-01 10:22:14 -0400

    auctex: switch back to zathura

--- .emacs.d/init-auctex.el
+++ .emacs.d/init-auctex.el
@@ -9,7 +9,10 @@
     (setenv "PATH" (concat (getenv "PATH") "/Library/TeX/texbin:/usr/local/bin"))
     (setq exec-path (append exec-path '("/Library/TeX/texbin" "/usr/local/bin"))))
   (progn
-    (setq TeX-view-program-selection '((output-pdf "PDF Tools"))
-          TeX-source-correlate-start-server t)
-    (add-hook 'TeX-after-compilation-finished-functions
-            #'TeX-revert-document-buffer)))
+    ;; (setq TeX-view-program-selection '((output-pdf "PDF Tools"))
+    ;;       TeX-source-correlate-start-server t
+    ;;       doc-view-resolution 300)
+    ;; (add-hook 'TeX-after-compilation-finished-functions
+    ;;         #'TeX-revert-document-buffer)))
+    (setq TeX-view-program-list '(("zathura" "zathura %o"))
+          TeX-view-program-selection '((output-pdf "zathura")))))
