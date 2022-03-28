3fe1352f0494675d9de816eb35ecbbc6356885f5
Brandon Amos <brandon.amos.cs@gmail.com>
2022-03-28 11:56:34 -0700

    xmobar: add deadlines

--- .xmonad/xmobar.py
+++ .xmonad/xmobar.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-from datetime import datetime
+from datetime import datetime, date
 import psutil
 import sys
 import argparse
@@ -12,12 +12,21 @@ args = parser.parse_args()
 
 status = ''
 
+deadlines = {
+    'NeurIPS': date(2022, 5, 19)
+}
+
 if args.direction == 'left':
     status = '<fc=#B27AEB><fn=2>❤</fn></fc>'
 elif args.direction == 'right':
     battery = psutil.sensors_battery()
     if not battery.power_plugged:
         status += f'<fc=#D43737><fn=1></fn>{int(battery.percent)}%</fc> '
+
+    today_date = datetime.now().date()
+    for tag,tag_date in deadlines.items():
+        n_days = (tag_date-today_date).days
+        status += f'<fc=#ABABAB>{tag}: {n_days} days | </fc>'
 
     now = datetime.now()
     time_str = now.strftime('%Y.%m.%d %-I:%M%p')
