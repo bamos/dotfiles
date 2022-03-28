6912c8266384372947aa0a66b759e215b37799f4
Brandon Amos <brandon.amos.cs@gmail.com>
2022-03-28 11:56:41 -0700

    tweak monitors

--- .xmonad/startup.sh
+++ .xmonad/startup.sh
@@ -21,12 +21,12 @@ xrandr --auto
 xrandr --output eDP-1 --mode 1280x720
 
 # HDMI-2 connected 1920x810+0+0 (normal left inverted right x axis y axis) 673mm x 284mm
-# if [[ $(xrandr | grep 'HDMI-2 connected') ]]; then
-#   xrandr --newmode "1920x810_60.00"  127.50  1920 2024 2224 2528  810 813 823 841 -hsync +vsync
-#   xrandr --addmode HDMI-2 1920x810_60.00
-#   xrandr --output HDMI-2 --mode 1920x810_60.00
-#   xrandr --output HDMI-2 --left-of eDP-1
-if [[ $(xrandr | grep 'DP-1 connected.*673mm') ]]; then
+if [[ $(xrandr | grep 'HDMI-2 connected') ]]; then
+  xrandr --newmode "1920x810_60.00"  127.50  1920 2024 2224 2528  810 813 823 841 -hsync +vsync
+  xrandr --addmode HDMI-2 1920x810_60.00
+  xrandr --output HDMI-2 --mode 1920x810_60.00
+  xrandr --output HDMI-2 --left-of eDP-1
+elif [[ $(xrandr | grep 'DP-1 connected.*673mm') ]]; then
   xrandr --newmode "1920x810_60.00"  127.50  1920 2024 2224 2528  810 813 823 841 -hsync +vsync
   xrandr --addmode DP-1 1920x810_60.00
   xrandr --output DP-1 --mode 1920x810_60.00
