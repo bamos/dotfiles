a192a868383f9b9406fcf27e0de8c90c7840b54e
Brandon Amos <brandon.amos.cs@gmail.com>
2022-04-09 14:00:24 -0400

    tweak monitors/xmobar

--- .xmonad/startup.sh
+++ .xmonad/startup.sh
@@ -20,8 +20,10 @@ xset r 66 # Make the remapped capslock repeat
 xrandr --auto
 xrandr --output eDP-1 --mode 1280x720
 
-# HDMI-2 connected 1920x810+0+0 (normal left inverted right x axis y axis) 673mm x 284mm
+pkill xmobar
+
 if [[ $(xrandr | grep 'HDMI-2 connected') ]]; then
+  # cvt 1920 810 60
   xrandr --newmode "1920x810_60.00"  127.50  1920 2024 2224 2528  810 813 823 841 -hsync +vsync
   xrandr --addmode HDMI-2 1920x810_60.00
   xrandr --output HDMI-2 --mode 1920x810_60.00
@@ -31,9 +33,25 @@ elif [[ $(xrandr | grep 'DP-1 connected.*673mm') ]]; then
   xrandr --addmode DP-1 1920x810_60.00
   xrandr --output DP-1 --mode 1920x810_60.00
   xrandr --output DP-1 --left-of eDP-1
-elif [[ $(xrandr | grep 'DP-1 connected') ]]; then
-  xrandr --output DP-1 --mode 1280x800
-  xrandr --output eDP-1 --same-as DP-1
+elif [[ $(xrandr | grep 'DP-1 connected.*597mm') ]]; then
+  xrandr --output DP-1 --mode 1920x1080
+  if [[ $(cat /proc/acpi/button/lid/LID/state | grep open) ]]; then
+    xrandr --output DP-1 --left-of eDP-1
+  else
+    xrandr --output eDP-1 --same-as DP-1
+    xmobar ~/.xmonad/xmobar.hs -x 1 &
+  fi
+elif [[ $(xrandr | grep 'DP-1 connected.*401mm') ]]; then
+  xrandr --output DP-1 --mode 1920x1080
+  if [[ $(cat /proc/acpi/button/lid/LID/state | grep open) ]]; then
+    xrandr --output DP-1 --left-of eDP-1
+  else
+    xrandr --output eDP-1 --same-as DP-1
+    xmobar ~/.xmonad/xmobar.hs -x 1 &
+  fi
+# elif [[ $(xrandr | grep 'DP-1 connected') ]]; then
+#   xrandr --output DP-1 --mode 1280x800
+#   xrandr --output eDP-1 --same-as DP-1
 fi
 
 # https://www.reddit.com/r/archlinux/comments/8n3iye
