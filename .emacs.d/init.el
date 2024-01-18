437e1736f2eefd55cc6ccc670e2d91c2f7208550
Brandon Amos <brandon.amos.cs@gmail.com>
2024-01-18 06:48:05 +0000

    emacs: setq->setq-default

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -44,10 +44,10 @@
 
 (require 'magit)
 (global-set-key (kbd "C-x C-g") 'magit-status)
-(setq magit-push-always-verify nil
-      magit-last-seen-setup-instructions "1.4.0")
+(setq-default magit-push-always-verify nil
+	      magit-last-seen-setup-instructions "1.4.0")
 
-(setq ring-bell-function 'ignore)
+(setq-default ring-bell-function 'ignore)
 
 (unless window-system
   (require 'mouse)
@@ -55,23 +55,22 @@
   (global-set-key [mouse-4] '(lambda () (interactive) (scroll-down 1)))
   (global-set-key [mouse-5] '(lambda () (interactive) (scroll-up 1)))
   (defun track-mouse (e))
-  (setq mouse-sel-mode t)
+  (setq-default mouse-sel-mode t)
 )
 
 (add-hook 'find-file-hook (lambda () (linum-mode 0))) ; Line numbers.
 (set-face-foreground 'minibuffer-prompt "white") ; White miniprompt.
 (fset 'yes-or-no-p 'y-or-n-p) ; yes/no -> y/n
-(setq vc-follow-symlinks t) ; Always follow symlinks.
+(setq-default vc-follow-symlinks t) ; Always follow symlinks.
 ;; (add-hook 'emacs-startup-hook  'delete-other-windows)
-(setq inhibit-startup-message t)
-(setq make-backup-files nil)
+(setq-default inhibit-startup-message t)
+(setq-default make-backup-files nil)
 (show-paren-mode 1)
-(setq show-paren-delay 0)
+(setq-default show-paren-delay 0)
 
-(setq shell-prompt-pattern ".*» *")
-;; (setq dired-use-ls-dired nil)
+(setq-default shell-prompt-pattern ".*» *")
 
-(setq show-trailing-whitespace t)
+(setq-default show-trailing-whitespace t)
 ;; (add-hook 'before-save-hook 'delete-trailing-whitespace)
 (defun save-buffer-without-dtw () ; http://stackoverflow.com/questions/14913398
   (interactive)
@@ -84,13 +83,13 @@
             (save-buffer)))))))
 
 (require 'saveplace)
-(setq save-place-file (concat user-emacs-directory "saveplace.el") )
-(setq save-place t)
+(setq-default save-place-file (concat user-emacs-directory "saveplace.el") )
+(setq-default save-place t)
 
 (menu-bar-mode -1)
 (tool-bar-mode -1)
 (scroll-bar-mode -1)
-(setq mode-line-format nil)
+(setq-default mode-line-format nil)
 
 (transient-mark-mode 1)
 
@@ -99,16 +98,16 @@
   (if xterm-mouse-mode (xterm-mouse-mode 1)))
 (add-hook 'after-make-frame-functions 'my-terminal-config)
 
-(setq compilation-scroll-output 'first-error)
+(setq-default compilation-scroll-output 'first-error)
 
 ;; default tab width to 2 except for python
-(setq indent-tabs-mode nil)
-(setq tab-width 4)(setq sh-basic-offset 4)(setq sh-indentation 4)
+(setq-default indent-tabs-mode nil)
+(setq-default tab-width 4)(setq-default sh-basic-offset 4)(setq-default sh-indentation 4)
 ;; (add-hook 'python-mode-hook (lambda() (
-;;   (setq tab-width 4)(setq sh-basic-offset 4)(setq sh-indentation 4))))
+;;   (setq-default tab-width 4)(setq-default sh-basic-offset 4)(setq-default sh-indentation 4))))
 
 (require 'yasnippet)
-(setq yas-snippet-dirs
+(setq-default yas-snippet-dirs
       '("~/.emacs.d/snippets/yasnippet-snippets"
         "~/.emacs.d/snippets/yasmate/snippets"
         "~/.emacs.d/snippets/custom"
@@ -128,18 +127,18 @@
 (global-unset-key (kbd "C-x C-t"))
 
 
-(setq initial-scratch-message "")
+(setq-default initial-scratch-message "")
 
-(setq custom-file "~/.emacs.d/custom.el")
+(setq-default custom-file "~/.emacs.d/custom.el")
 (load custom-file 'noerror)
 
 (require 'darkroom)
-(setq darkroom-margins 0.1)
+(setq-default darkroom-margins 0.1)
 ;; (add-hook 'LaTeX-mode-hook 'darkroom-mode)
 
 ;; OSX-specific:
 (setenv "PATH" "/usr/local/bin:/Library/TeX/texbin/:$PATH" t)
-(setq exec-path (append exec-path '("/Library/TeX/texbin")))
+(setq-default exec-path (append exec-path '("/Library/TeX/texbin")))
 
 (when (string= system-name "bda-mbp")
     (set-face-attribute 'default (selected-frame) :height 120))
