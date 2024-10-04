c3afd3052ff26d8201b0fd3c2a44d239ef407e3d
Brandon Amos <brandon.amos.cs@gmail.com>
2024-10-04 00:26:19 -0400

    tweak monitor

--- .xmonad/startup.sh
+++ .xmonad/startup.sh
@@ -33,12 +33,12 @@ if [[ $(xrandr | grep 'HDMI-2 connected') ]]; then
   xrandr --addmode HDMI-2 1920x810_60.00
   xrandr --output HDMI-2 --mode 1920x810_60.00
   xrandr --output HDMI-2 --left-of eDP-1
-elif [[ $(xrandr | grep 'DP-1 connected.*673mm') ]]; then
-  xrandr --newmode "1920x810_60.00"  127.50  1920 2024 2224 2528  810 813 823 841 -hsync +vsync
-  xrandr --addmode DP-1 1920x810_60.00
-  xrandr --output DP-1 --mode 1920x810_60.00
-  xrandr --output DP-1 --left-of eDP-1
-elif [[ $(xrandr | grep 'DP-1 connected.*697mm') ]]; then
+# elif [[ $(xrandr | grep 'DP-1 connected.*673mm') ]]; then
+#   xrandr --newmode "1920x810_60.00"  127.50  1920 2024 2224 2528  810 813 823 841 -hsync +vsync
+#   xrandr --addmode DP-1 1920x810_60.00
+#   xrandr --output DP-1 --mode 1920x810_60.00
+#   xrandr --output DP-1 --left-of eDP-1
+elif [[ $(xrandr | grep 'DP-1 connected') ]]; then
   xrandr --output DP-1 --mode 1920x1080
   if [[ $(cat /proc/acpi/button/lid/LID/state | grep open) ]]; then
     xrandr --output DP-1 --left-of eDP-1
