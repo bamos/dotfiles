1920931fb072f76bf3ce4d9f4561fface76e3a4c
Brandon Amos <brandon.amos.cs@gmail.com>
2025-03-15 14:28:07 -0400

    auctex: switch pdf viewer

--- .emacs.d/init-auctex.el
+++ .emacs.d/init-auctex.el
@@ -4,8 +4,11 @@
 
 (if (eq system-type 'darwin)
   (progn
-    (setq TeX-view-program-list '(("open" "open %o")))
-    (setq TeX-view-program-selection '((output-pdf "open")))
+    ;; (setq TeX-view-program-list '(("open" "open %o")))
+    ;; (setq TeX-view-program-selection '((output-pdf "open")))
+    (setq TeX-view-program-selection '((output-pdf "PDF Viewer")))
+    (setq TeX-view-program-list
+     '(("PDF Viewer" "/Applications/Skim.app/Contents/SharedSupport/displayline -b -g %n %o %b")))
     (setenv "PATH" (concat (getenv "PATH") "/Library/TeX/texbin:/usr/local/bin"))
     (setq exec-path (append exec-path '("/Library/TeX/texbin" "/usr/local/bin"))))
   (progn
